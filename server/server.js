ddc3d33f432516f60faa14c95befd30d6b341dea
unknown <shaik.rehamanrp@gmail.com>
2024-08-16 10:13:28 +0530

    express basic setup

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+
+app.listen(port, ()=>{
+    console.log(`Server started on port ${port}.`)
+})
+
+
+app.get("/", (req, res)=>{
+    res.send("<h1>Home page</h1>")
+});
