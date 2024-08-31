8c613fe922616588dbb7c67057bae9e801fb77f1
unknown <shaik.rehamanrp@gmail.com>
2024-08-31 12:40:11 +0530

    changed the directory structure for portfolio

--- server/controllers/portfolioController.js
+++ server/controllers/portfolioController.js
@@ -5,16 +5,16 @@ function sendPortfolioHtml(req, res) {
     res.sendFile(path.join(process.cwd(), 'public', 'templates/portfolio.html'));
 }
 function sendNewPortfolioHtml(req, res) {
-    res.render("newPortfolio.ejs");
+    res.render('portfolio/newPortfolio.ejs');
 }
 async function projects(req, res) {
     const projectList = await returnProjects(req)
-    res.render("projects.ejs", { "projects": projectList });
+    res.render("portfolio/projects.ejs", { "projects": projectList });
 }
 async function projectDetails(req, res) {
     const metaUrl = req.params.metaUrl
     const project = await returnProject(metaUrl)
-    res.render("projectDetails.ejs", { "project": project });
+    res.render("portfolio/projectDetails.ejs", { "project": project });
 }
 
 export { sendPortfolioHtml, sendNewPortfolioHtml, projects, projectDetails, returnProject }
