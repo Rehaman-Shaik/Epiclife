2333c58bd2916176ae2ae597b68a28e83fe95a19
unknown <shaik.rehamanrp@gmail.com>
2024-09-01 16:09:49 +0530

    removed the old portfolio and replaced with the new portfolip

--- server/controllers/portfolioController.js
+++ server/controllers/portfolioController.js
@@ -1,11 +1,7 @@
-import path from 'path';
 import { returnProjects, returnProject } from '../services/portfolioServices.js';
 
 function sendPortfolioHtml(req, res) {
-    res.sendFile(path.join(process.cwd(), 'public', 'templates/portfolio.html'));
-}
-function sendNewPortfolioHtml(req, res) {
-    res.render('portfolio/newPortfolio.ejs');
+    res.render('portfolio/portfolio.ejs');
 }
 async function projects(req, res) {
     const projectList = await returnProjects(req)
@@ -17,4 +13,4 @@ async function projectDetails(req, res) {
     res.render("portfolio/projectDetails.ejs", { "project": project });
 }
 
-export { sendPortfolioHtml, sendNewPortfolioHtml, projects, projectDetails, returnProject }
+export {sendPortfolioHtml, projects, projectDetails, returnProject }
--- server/routes/portfolioRouter.js
+++ server/routes/portfolioRouter.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { sendPortfolioHtml, sendNewPortfolioHtml, projects, projectDetails} from "../controllers/portfolioController.js";
+import {sendPortfolioHtml, projects, projectDetails} from "../controllers/portfolioController.js";
 
 const portfolioRouter = express.Router();
 
 portfolioRouter.get("/portfolio", sendPortfolioHtml)
-portfolioRouter.get("/new-portfolio", sendNewPortfolioHtml)
 portfolioRouter.get("/portfolio/projects", projects)
 portfolioRouter.get("/portfolio/project/:metaUrl", projectDetails)
 
